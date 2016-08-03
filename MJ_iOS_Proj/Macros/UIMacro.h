7acabe93451d6d56bfd6b55c5db5890c0cfb8b9b
Minjie <mjopenglsdl@gmail.com>
2016-08-03 15:13:55 +0800

    basic widget definitions

--- MJ_iOS_Proj/Macros/UIMacro.h
+++ MJ_iOS_Proj/Macros/UIMacro.h
@@ -13,7 +13,25 @@
 #define SCREEN_WIDTH     [UIScreen mainScreen].bounds.size.width
 #define SCREEN_HEIGHT   [UIScreen mainScreen].bounds.size.height
 
-#define COMMON_WIDGET_OFFSET SCREEN_HEIGHT*0.1
+// status bar
+#define STATUS_BAR_SIZE     [UIApplication sharedApplication].statusBarFrame.size
+#define STATUS_BAR_WIDTH    MAX(STATUS_BAR_SIZE.height, STATUS_BAR_SIZE.width)
+#define STATUS_BAR_HEIGHT   MIN(STATUS_BAR_SIZE.height, STATUS_BAR_SIZE.width)
+
+// nav bar
+#define NAV_BAR_SIZE     self.navigationController.navigationBar.frame.size
+#define NAV_BAR_WIDTH    NAV_BAR_SIZE.width
+#define NAV_BAR_HEIGHT  NAV_BAR_SIZE.height
+
+// Top bar total height
+#define NAV_AND_STATUS_BAR_HEIGHT (NAV_BAR_HEIGHT+STATUS_BAR_HEIGHT)
+
+/// widgets dimensions
+#define WIDGET_COMMON_OFFSET (SCREEN_HEIGHT*0.1)
+#define WIDGET_HORI_MARGIN 20.0f
+
+#define TEXTFIELD_HEIGHT 34.0f
+#define BUTTON_HEIGHT 40.0f
 
 
 #endif /* UIMacro_h */
--- MJ_iOS_Proj/VC/InfoVC.m
+++ MJ_iOS_Proj/VC/InfoVC.m
@@ -11,6 +11,8 @@
 #import "Masonry.h"
 #import "UIMacro.h"
 
+#define BTN_ADD_WIDTH 40.0f
+
 
 @implementation InfoVC
 
@@ -25,11 +27,8 @@
     // ui
     self.view.backgroundColor=[UIColor grayColor];
     self.navigationItem.title=@"Info";
-    
-    UITextField *txtFieldName=[[UITextField alloc]initWithPlaceHolder:@"Your Name"];
-    [txtFieldName mas_makeConstraints:^(MASConstraintMaker *make) {
-        ;
-    }];
+
+    [self setupUI];
 
     // gesture
     UISwipeGestureRecognizer *swipeLeft=[[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(action_swipeLeft:)];
@@ -38,6 +37,53 @@
     
 }
 
+
+#pragma mark - UI 
+- (void)setupUI
+{
+    UITextField *txtfName=[[UITextField alloc]initWithPlaceHolder:@"Your Name"];
+    UITextView *txtvContent=[[UITextView alloc]init];
+    
+    UIButton *btnAdd=[[UIButton alloc]init];
+    [btnAdd setTitle:@"Add" forState:UIControlStateNormal];
+    
+    UIButton *btnSave=[[UIButton alloc]init];
+    [btnSave setTitle:@"Save" forState:UIControlStateNormal];
+    
+    [self.view addSubview:txtfName];
+    [self.view addSubview:txtvContent];
+    [self.view addSubview:btnAdd];
+    [self.view addSubview:btnSave];
+
+
+    [txtfName mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view.mas_top).mas_offset(NAV_AND_STATUS_BAR_HEIGHT+WIDGET_COMMON_OFFSET);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(WIDGET_HORI_MARGIN);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-WIDGET_HORI_MARGIN);
+        make.height.mas_equalTo(TEXTFIELD_HEIGHT);
+    }];
+    
+    [txtvContent mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(txtfName.mas_bottom).mas_offset(WIDGET_COMMON_OFFSET);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(WIDGET_HORI_MARGIN);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-WIDGET_HORI_MARGIN);
+        make.height.mas_equalTo(TEXTFIELD_HEIGHT*2);
+    }];
+    
+    [btnAdd mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(txtvContent.mas_bottom).mas_offset(WIDGET_COMMON_OFFSET);
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.width.mas_equalTo(BTN_ADD_WIDTH);
+        make.height.mas_equalTo(BTN_ADD_WIDTH);
+    }];
+    
+    [btnSave mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(btnAdd.mas_bottom).mas_offset(WIDGET_COMMON_OFFSET);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(WIDGET_HORI_MARGIN);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-WIDGET_HORI_MARGIN);
+        make.height.mas_equalTo(BUTTON_HEIGHT);
+    }];
+}
 
 #pragma mark - Actions
 - (void)action_swipeLeft:(id)sender
